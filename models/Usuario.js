cb2921562688ba9d93a1983932980adde0cba95f
LeninHernandez <90071666+lenin123333@users.noreply.github.com>
2024-01-18 15:05:22 -0600

    Changes to be committed: 	modified:   models/Usuario.js

--- models/Usuario.js
+++ models/Usuario.js
@@ -19,13 +19,14 @@ const Usuario = db.define('usuarios', {
     confirmado: DataTypes.BOOLEAN,
     imagen: {
         type: DataTypes.STRING,
-        allowNull: true // O el valor que desees
+        allowNull: true 
     }
 },{
     hooks:{
         beforeCreate:async function (usuario) {
             const salt = await bcrypt.genSalt(10)
             usuario.password= await bcrypt.hash(usuario.password,salt)
+            
         }
     },
     scopes:{
@@ -37,18 +38,7 @@ const Usuario = db.define('usuarios', {
     }
 });
 
-// Ejecutar la consulta SQL para agregar la columna imagen
-const agregarColumnaImagen = async () => {
-    try {
-        await db.query('ALTER TABLE usuarios ADD COLUMN imagen VARCHAR(255)');
-        console.log('Columna imagen agregada correctamente');
-    } catch (error) {
-        console.error('Error al agregar la columna imagen: ', error);
-    }
-};
 
-// Llamar a la función para agregar la columna imagen
-agregarColumnaImagen();
 
 // Métodos personalizados
 Usuario.prototype.verificarPassword = function(password){
