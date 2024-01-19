5a855c60b9605528d9d571129cb478f33af959fa
LeninHernandez <90071666+lenin123333@users.noreply.github.com>
2024-01-18 18:54:45 -0600

    Changes to be committed: 	modified:   helpers/emails.js

--- helpers/emails.js
+++ helpers/emails.js
@@ -31,7 +31,7 @@ const emailRegistro = async(datos) =>{
 const emailOlvidePassword = async(datos) =>{
   const transport = nodemailer.createTransport({
       host: process.env.EMAIL_HOST ,
-      port: process.env.PORT,
+      port: process.env.EMAIL_PORT,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD
